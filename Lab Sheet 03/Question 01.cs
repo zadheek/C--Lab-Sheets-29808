20d82a7f6f323ddcaedc951766a503d54cd58e1f
Sadee <141108828+zadheek@users.noreply.github.com>
2024-04-25 14:46:28 +0530

    Add files via upload

---
+++ Lab Sheet 03/Question 01.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    internal class Program
+    {
+          class Course
+{
+    private string courseName;
+    private string instructorName;
+    private double grade;
+
+    public string CourseName
+    {
+        get { return courseName; }
+    }
+
+    public double Grade
+    {
+        get
+        {
+            if (grade < 0 || grade > 100)
+                throw new Exception("Invalid grade");
+            return grade;
+        }
+        private set
+        {
+            if (value < 0 || value > 100)
+                throw new Exception("Invalid grade");
+            grade = value;
+        }
+    }
+
+    public void SetInstructorName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new Exception("Invalid instructor name");
+        instructorName = name;
+    }
+
+    private string CalculateLetterGrade()
+    {
+        if (grade >= 90) return "A";
+        else if (grade >= 80) return "B";
+        else if (grade >= 70) return "C";
+        else if (grade >= 60) return "D";
+        else return "F";
+    }
+
+    public void PrintCourseInfo()
+    {
+        Console.WriteLine($"Course Name: {CourseName}");
+        Console.WriteLine($"Instructor Name: {instructorName}");
+        Console.WriteLine($"Letter Grade: {CalculateLetterGrade()}");
+    }
+}
+    
+    }
+}
---
+++ Lab Sheet 03/Question 02.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    internal class Program
+    {
+        class Employee
+        {
+            public int employeeID { get; }
+            public string FullName { get; set; }
+            private double salary;
+
+            public Employee(int id, string name, double salary)
+            {
+                employeeID = id;
+                FullName = name;
+                this.salary = salary;
+            }
+
+            public void DisplayEmployeeInfo()
+            {
+                Console.WriteLine($"Employee ID: {employeeID}");
+                Console.WriteLine($"Full Name: {FullName}");
+                Console.WriteLine($"Salary: {salary}");
+            }
+        }
+
+        class Progam
+        {
+            static void Main(string[] args)
+            {
+                Employee employee = new Employee(101, "John Doe", 50000);
+
+
+                Console.WriteLine($"Employee ID: {employee.employeeID}");
+
+
+                employee.FullName = "Jane Smith";
+
+                employee.DisplayEmployeeInfo();
+            }
+        }
+    }
+}
+
